0490dd2181e5bf488888d99feb40f2f68a87ba5b
= <=>
2015-12-21 19:47:32 +0800

    commit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'nokogiri'
 gem 'sprockets'
 gem 'seo_helper', '~> 1.0'
 gem 'rails-i18n'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,6 +25,9 @@ module ApplicationHelper
 
   end
 
+  
+
+
   def render_cart_items_count(cart)
     cart.items.count
   end
