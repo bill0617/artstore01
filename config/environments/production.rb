af7794fdaa7a16d82dc369e62942a4d6d686fd30
= <kingliin814@gmail.com>
2016-01-04 18:34:14 +0800

    try000

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => "ap-northeast-1"
+      :region => "us-east-1"
     }
   }
   Paperclip::Attachment.default_options[:use_timestamp] = false
