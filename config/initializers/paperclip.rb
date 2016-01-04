38b90be37dc0321659d8b85440c00551030dea2e
= <kingliin814@gmail.com>
2016-01-04 17:35:43 +0800

    try6

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 Paperclip::Attachment.default_options[:url] = 's3-us-west-1.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/000/000/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
